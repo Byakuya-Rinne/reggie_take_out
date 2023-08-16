88ef25ff30d2f4ffbaf629850479ee742143f888
Yuki <2353845636@qq.com>
2023-08-16 12:13:56 +0800

    新增菜品

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/category")
 @Slf4j
@@ -66,7 +68,19 @@ public class CategoryController {
     }
 
 
+    //查询分类
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        //条件构造器
+        LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        //添加条件
+        lambdaQueryWrapper.eq(category.getType()!=null, Category::getType, category.getType());
+        //添加排序条件
+        lambdaQueryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
 
+        List<Category> list = categoryService.list(lambdaQueryWrapper);
+        return R.success(list);
+    }
 
 
 
--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -1,13 +1,47 @@
 package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+    //新增菜品, 保存口味数据
+    @Transactional
+    @Override
+    public void saveWithFlavor(DishDto dishDto) {
+        //保存菜品基本信息到dish表
+        this.save(dishDto);
+
+        //获取菜品id
+        Long dishId = dishDto.getId();
+
+        //TODO 不懂(item)->{ item.setDishId(dishId); return item; }).collect(Collectors.toList());
+        //获取菜品口味
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors.stream().map((item)->{
+            item.setDishId(dishId);
+            return item;
+        }).collect(Collectors.toList());
+
+        //保存口味数据到dish_flavor表
+        dishFlavorService.saveBatch(flavors);//批量保存
+
+    }
 }
