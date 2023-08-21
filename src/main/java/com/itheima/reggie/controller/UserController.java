24d380736d205942d3f77eddb1cbe0e1d6626523
Yuki <2353845636@qq.com>
2023-08-21 17:14:07 +0800

    移动端用户验证码发送与登录

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -59,7 +59,8 @@ public class UserController {
      */
     @PostMapping("/login")
     public R<User> login(@RequestBody Map map, HttpSession session){
-        log.info(map.toString());
+        //可以新建dto, 也可用map接收前端键值对的数据
+        log.info("测试, 前端传入的map内容为: {}", map.toString());
 
         //获取手机号
         String phone = map.get("phone").toString();
@@ -73,7 +74,6 @@ public class UserController {
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
         if(codeInSession != null && codeInSession.equals(code)){
             //如果能够比对成功，说明登录成功
-
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
 
@@ -82,7 +82,7 @@ public class UserController {
                 //判断当前手机号对应的用户是否为新用户，如果是新用户就自动完成注册
                 user = new User();
                 user.setPhone(phone);
-                user.setStatus(1);
+                user.setStatus(1); //1启用, 0禁用
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
