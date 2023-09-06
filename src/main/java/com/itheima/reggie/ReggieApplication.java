5360d42b3eb5268fa979488989b84a15a6dbe290
Yuki <2353845636@qq.com>
2023-09-06 11:16:11 +0800

    lambda表达式练习

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -7,14 +7,42 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Slf4j
-@SpringBootApplication
-@EnableTransactionManagement
-@ServletComponentScan
-@EnableCaching
+import java.util.function.IntBinaryOperator;
+
+//
+//@Slf4j
+//@SpringBootApplication
+//@EnableTransactionManagement
+//@ServletComponentScan
+//@EnableCaching
 public class ReggieApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ReggieApplication.class,args);
-        log.info("项目启动成功...");
+        //SpringApplication.run(ReggieApplication.class,args);
+        //log.info("项目启动成功...");
+
+        int i = calculate(new IntBinaryOperator() {
+            @Override
+            public int applyAsInt(int left, int right) {
+                return left + right;
+            }
+        });
+
+        System.out.println(i);
+
     }
+
+
+
+    public static int calculate(IntBinaryOperator operator){
+        int a = 111;
+        int b = 222;
+        return operator.applyAsInt(a,b);
+    }
+
+
+
+
+
+
+
 }
