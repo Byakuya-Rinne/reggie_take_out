e387ddb0fa164fcdbb5714abde262d02ea25010a
Yuki <2353845636@qq.com>
2023-09-07 17:01:47 +0800

    CSV解析器

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -1,3 +1,4 @@
+/*
 package com.itheima.reggie;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,12 +21,8 @@ public class ReggieApplication {
         //SpringApplication.run(ReggieApplication.class,args);
         //log.info("项目启动成功...");
 
-        int i = calculate(new IntBinaryOperator() {
-            @Override
-            public int applyAsInt(int left, int right) {
-                return left + right;
-            }
-        });
+        
+        int i = calculate(Integer::sum);
 
         System.out.println(i);
 
@@ -46,3 +43,83 @@ public class ReggieApplication {
 
 
 }
+*/
+
+
+
+package com.itheima.reggie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReggieApplication {
+    public static List<List<String>> parseCSV(String csvStr) {
+        List<List<String>> result = new ArrayList<>();
+
+        String[] lines = csvStr.split("\n");
+
+        for (String line : lines) {
+            List<String> row = new ArrayList<>();
+            boolean inQuotes = false;
+            StringBuilder field = new StringBuilder();
+
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                if (c == '"') {
+                    inQuotes = !inQuotes;
+                } else if (c == ',' && !inQuotes) {
+                    row.add(field.toString());
+                    field.setLength(0);
+                } else {
+                    field.append(c);
+                }
+            }
+
+            row.add(field.toString());
+            result.add(row);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String csvStr = "Name,Age,Address\nJohn Doe,30,\"123 Main Street, Apartment 4B\"\nAlice Smith,25,Kwai Plaza\nMary Johnson,35,\"456 Elm Avenue, Suite 10C\"";
+        List<List<String>> parsedCSV = parseCSV(csvStr);
+
+        // 打印解析后的CSV内容
+        for (List<String> row : parsedCSV) {
+            System.out.println(row);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
